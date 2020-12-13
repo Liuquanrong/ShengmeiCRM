88b12e8e47c009453e174c749a87b37787d9ab13
quanrong <quanrong@163.com>
2020-12-13 15:42:14 +0800

    First Commit

---
+++ CRM/src/main/java/com/quanrong/Exception/LoginException.java
@@ -0,0 +1,11 @@
+package com.quanrong.Exception;
+
+public class LoginException extends Exception{
+    public LoginException() {
+        super();
+    }
+
+    public LoginException(String message) {
+        super(message);
+    }
+}
